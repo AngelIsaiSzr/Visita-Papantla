5cf9bb0787eafb9f3badb31b46a0918bf1e37d8a
AngelIsaiSzr <angelmoreno152000@gmail.com>
2024-11-16 11:29:31 -0600

    Se incluyo el inc

---
+++ assets/inc/footer.php
@@ -0,0 +1,173 @@
+<!-- Footer Section Start -->
+<footer class="footer-wrap">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row ptb-60">
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 pe-xl-5">
+                    <div class="footer-widget">
+                        <a href="index.php" class="footer-logo">
+                            <img src="assets/img/logo-dark.png" alt="Image">
+                        </a>
+                        <p class="comp-desc">
+                            En Visita Papantla nuestra misión es hacer que descubrir y reservar hoteles sea un
+                            proceso sencillo y rápido para que cada visitante disfrute al máximo.
+                        </p>
+                        <div class="newsletter-form contact-form">
+                            <form class="subscribe__form" action="assets/php/mail.php" method="post">
+
+                                <div class="col-12">
+                                    <input type="email" placeholder="Correo Electrónico" required>
+                                </div>
+
+                                <div class="col-12">
+                                    <button type="submit">Suscribirse ahora</button>
+                                </div>
+
+                                <!-- Start Form Message -->
+                                <div class="col-12">
+                                    <div class="msg_submit text-center alert alert-success successform subscribe__msg"
+                                        role="alert">
+                                        Su suscripción fue activada exitosamente!
+                                    </div>
+                                </div>
+                                <!-- End Form Message -->
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 ps-xl-5 ps-lg-4 ps-md-5">
+                    <div class="footer-widget">
+                        <h3 class="footer-widget-title">Nuestros Servicios</h3>
+                        <ul class="footer-menu list-style">
+                            <li>
+                                <a>
+                                    Reservaciones
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Hoteles
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Restaurantes
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Historia de Papantla
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Galería de Papantla
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Blog de Papantla
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6">
+                    <div class="footer-widget">
+                        <h3 class="footer-widget-title">Nuestro Equipo</h3>
+                        <ul class="footer-menu list-style">
+                            <li>
+                                <a href="#">
+                                    Angel Salazar
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Valeria Rivera
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Denisse Nava
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Jamin García
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Emir Jiménez
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Jair Percastre
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 ps-md-5">
+                    <div class="footer-widget">
+                        <h3 class="footer-widget-title">Información de Contacto</h3>
+                        <ul class="contact-info list-style">
+                            <li>
+                                <i class="bx bx-location-plus"></i>
+                                <h6>Ubicación</h6>
+                                <p>Papantla de Olarte, Veracruz, MX.</p>
+                            </li>
+                            <li>
+                                <i class="bx bx-envelope"></i>
+                                <h6>Correo</h6>
+                                <a href="mailto:contacto@visitapapantla.com">contacto@visitapapantla.com</a>
+                            </li>
+                            <li>
+                                <i class="bx bx-phone-call"></i>
+                                <h6>Télefono</h6>
+                                <a href="tel:+527848881891">+52 784 888 1891</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-8 col-md-6 col-sm-7">
+                    <p class="copyright-text">Copyright © 2024 - Visita Papantla - Todos los Derechos
+                        Reservados.</p>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-5">
+                    <ul class="social-profile style1 list-style">
+                        <li>
+                            <a href="#">
+                                <i class="ri-facebook-fill"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="ri-twitter-fill"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="ri-instagram-line"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="ri-linkedin-fill"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
